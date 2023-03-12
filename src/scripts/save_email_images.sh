efedee8a5311f257f193836bb71fd8738a50fc7f
Rahul Yerrabelli <ryerrabelli@gmail.com>
2023-03-12 13:15:55 -0500

    Updated README and scripts to match my latest template across repos

---
+++ src/scripts/save_email_images.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# To change the permissions of this file so it can be run in terminal, do
+# chmod 755 <filename>.sh
+# chmod 755 src/scripts/save_email_images.sh
+# to run
+# ./src/scripts/save_email_images.sh
+#
+# This file allows you to include email address in a GitHub repo, publically
+# available to everyone, while still preventing it from easily being accessed
+# by web scrapers for spam email.
+
+SAVETOFILENAME="email-address-image.png"
+SAVETOFILEPATH="docs/$SAVETOFILENAME"
+
+IMAGETEXT="Rahul Sai Yerrabelli: \n  - ryerrab1@alumni.jh.edu \n  - rsy2@illinois.edu"
+
+echo "Saving email address(es) as an image. May take a few seconds."
+
+echo $IMAGETEXT | convert -background none -density 400 -resample 150 -unsharp 0x.5 -font "arial" text:- -trim +repage -bordercolor white -border 0 $SAVETOFILEPATH
+
+echo "Finished. Saved to file '$SAVETOFILEPATH'. On mac, to view the file with:"
+echo "  open $SAVETOFILEPATH -a Preview"
+
+ls -l $SAVETOFILEPATH
